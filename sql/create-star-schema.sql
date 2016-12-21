e744e0406b3c0f2d3c886201a505bfa6f29f14cd
kamaaa <kamillarmatys@gmail.com>
2016-12-21 22:08:37 +0100

    Update star schema, add partition and bitmap idex

--- sql/create-star-schema.sql
+++ sql/create-star-schema.sql
@@ -4,23 +4,8 @@
  * @date:        19/12/2016
 */
 
-CREATE TABLE g_sesja(
-  strona_id INT NOT NULL,
-  plec_id INT NOT NULL,
-  kraj_id INT NOT NULL,
-  wiek_id INT NOT NULL,
-  czas_id INT NOT NULL,
-  urzadzenie_id INT NOT NULL,
-  sr_czas_sesji NUMBER(10,4) DEFAULT 0 NOT NULL,
-  sum_czas_sesji INT DEFAULT 0 NOT NULL,
-  sum_liczba_klikniec INT DEFAULT 0 NOT NULL,
-  sr_liczba_klikniec NUMBER(10, 4) DEFAULT 0 NOT NULL,
-  sum_wizyt INT DEFAULT 0 NOT NULL,
-  CONSTRAINT g_sesja_pk PRIMARY KEY(strona_id, plec_id, kraj_id, wiek_id, czas_id, urzadzenie_id)
-);
-
-CREATE TABLE g_strony(
-  id INT NOT NULL CONSTRAINT g_strony_pk PRIMARY KEY,
+CREATE TABLE g_strona(
+  id INT NOT NULL CONSTRAINT g_strona_pk PRIMARY KEY,
   kategoria VARCHAR2(35) NOT NULL
 );
 
@@ -36,7 +21,7 @@ CREATE TABLE g_urzadzenia(
 );
 
 CREATE TABLE g_czas(
-  id INT NOT NULL CONSTRAINT g_czas_pk PRIMARY KEY,
+  id DATE NOT NULL CONSTRAINT g_czas_pk PRIMARY KEY,
   dzien NUMBER(2) NOT NULL,
   tydzien NUMBER(3) NOT NULL,
   miesiac NUMBER(2) NOT NULL,
@@ -49,6 +34,43 @@ CREATE TABLE g_kraj(
 );
 
 CREATE TABLE g_wiek(
-  int INT NOT NULL CONSTRAINT g_wiek_pk PRIMARY KEY,
+  id INT NOT NULL CONSTRAINT g_wiek_pk PRIMARY KEY,
   grupa_wiek CHAR(8) NOT NULL
 );
+
+CREATE TABLE g_sesja(
+  strona_id INT NOT NULL CONSTRAINT g_sesja_fk_g_strona REFERENCES g_strona(id),
+  plec_id INT NOT NULL CONSTRAINT g_sesja_fk_g_plec REFERENCES g_plec(id),
+  kraj_id INT NOT NULL CONSTRAINT g_sesja_fk_g_kraj REFERENCES g_kraj(id),
+  wiek_id INT NOT NULL CONSTRAINT g_sesja_fk_g_wiek REFERENCES g_wiek(id),
+  czas_id DATE NOT NULL CONSTRAINT g_sesja_fk_g_czas REFERENCES g_czas(id),
+  urzadzenie_id INT NOT NULL CONSTRAINT g_sesja_fk_g_urzadzenie REFERENCES g_urzadzenia(id),
+  sr_czas_sesji NUMBER(10,4) DEFAULT 0 NOT NULL,
+  sum_czas_sesji INT DEFAULT 0 NOT NULL,
+  sum_liczba_klikniec INT DEFAULT 0 NOT NULL,
+  sr_liczba_klikniec NUMBER(10, 4) DEFAULT 0 NOT NULL,
+  sum_wizyt INT DEFAULT 0 NOT NULL
+) PARTITION BY RANGE (czas_id)(
+    PARTITION lt_2010 VALUES LESS THAN (TO_DATE('01-01-2010', 'dd-mm-yyyy')),
+    PARTITION lt_2015 VALUES LESS THAN (TO_DATE('01-01-2015', 'dd-mm-yyyy')),
+    PARTITION lt_2020 VALUES LESS THAN (TO_DATE('01-01-2020', 'dd-mm-yyyy'))
+);
+
+-- create bitmap index for fact table
+CREATE BITMAP INDEX g_sesja_strona_bix ON g_sesja(strona_id) LOCAL;
+CREATE BITMAP INDEX g_sesja_plec_bix ON g_sesja(plec_id) LOCAL;
+CREATE BITMAP INDEX g_sesja_kraj_bix ON g_sesja(kraj_id) LOCAL;
+CREATE BITMAP INDEX g_sesja_wiek_bix ON g_sesja(wiek_id) LOCAL;
+CREATE BITMAP INDEX g_sesja_czas_bix ON g_sesja(czas_id) LOCAL;
+CREATE BITMAP INDEX g_sesja_urzadzenie_bix ON g_sesja(urzadzenie_id) LOCAL;
+
+-- bitmap index on web category
+CREATE BITMAP INDEX g_strona_kat_bix ON g_strona(kategoria);
+CREATE BITMAP INDEX g_plec_nazwa_bix ON g_plec(nazwa);
+
+-- join bitmap index 
+CREATE BITMAP INDEX g_sesja_g_strona ON g_sesja(g_strona.id)
+FROM g_sesja, g_strona
+WHERE g_sesja.strona_id = g_strona.id LOCAL;
+
+COMMIT;
